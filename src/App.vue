1e8a1830cb65adca82cfbf064f43a8a510e2a9b5
wangpeng <947034046@qq.com>
2017-12-08 18:25:04 +0800

    A Vue.js project

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="page">
+    <header>
+      <Topbar/>
+    </header>
+    <main>
+      <ResumeEditor/>
+      <ResumePreview/>
+    </main>
+  </div>
+</template>
+
+<script>
+import 'normalize.css/normalize.css'
+import './assets/reset.css' //因为 webpack 支持这样引入css
+
+import Topbar from './components/Topbar'
+import ResumeEditor from './components/ResumeEditor'
+import ResumePreview from './components/ResumePreview'
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      text: '哈喽'
+    }
+  },
+  components: {Topbar, ResumeEditor, ResumePreview}
+}
+</script>
+
+<style>
+
+  
+  .page {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background: #eaebec;
+    min-width: 1024px;
+  }
+  .page > main { flex-grow: 1; }
+
+  .page > main {
+    /* min-width: 1024px; */
+    max-width: 1440px;
+    margin-top: 16px;
+    margin-bottom: 16px;
+    display: flex;
+    justify-content: space-between;
+    padding: 0 16px;
+    width: 100%;
+    align-self: center;
+  }
+
+  #resumeEditor { width: 35%; }
+
+  #resumePreview {
+    flex-grow: 1;
+    margin-left: 16px;
+  }
+
+</style>
